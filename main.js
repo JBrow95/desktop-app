e85ffdb1dde6836029a123f14040e2e7e297afbd
Jbrown95 <browningjustin95@gmail.com>
2019-02-23 21:37:17 -0500

    added all files from copy

---
+++ main.js
@@ -0,0 +1,103 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+let addWindow;
+
+app.on('ready', function(){
+    // creates new window
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        frame: false
+    });
+
+    mainWindow.openDevTools({detach: true});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    mainWindow.once('ready-to-show', function(){
+        mainWindow.show();
+    });
+
+    // stop app when closed
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
+
+    // build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow(){
+     // creates new window
+     addWindow = new BrowserWindow({
+         width: 300,
+         height: 200,
+         title: 'add to do list item'
+     });
+
+     addWindow.loadURL(url.format({
+         pathname: path.join(__dirname, 'addWindow.html'),
+         protocol: 'file',
+         slashes: true
+     }));
+
+     // garbage collection
+     addWindow.on('close', function(){
+        addWindow = null;
+     });
+}
+
+
+// menu template 
+const mainMenuTemplate = [
+    {
+        label: 'file',
+        submenu: [
+            {
+                label: 'Add Games',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Remove Games'
+            },
+            {
+                label: 'Sign Out',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+// if mac, add empty object to menu
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+// add dev tools item if not in prod
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    });
+}
